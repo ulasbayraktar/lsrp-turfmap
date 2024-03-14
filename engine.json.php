4f539c4ddfdf9754070418d2dd9a916f46b1e04e
Montreal <ulaserenbyrktr@gmail.com>
2024-03-14 13:19:23 +0300

    LSRP Turfmap

--- engine.json.php
+++ engine.json.php
@@ -1,1 +1,73 @@
-//{"factions":[{"id":"917","name":"Little Tokyo","color":"1e1e1e","thread":"https:\/\/forum.lss-roleplay.com\/viewtopic.php?f=5&t=687985","turfIDs":["1881","2037"],"turfs":["2665 2506 2550 2506 2549 2582 2665 2582 ","4665 4506 4550 4506 4549 4582 4665 4582"],"turfcenter":[[2665,4819.5],[4607.25,4544]],"turfPolygon":[],"dimensions":[[372,199],[116,76]],"limits":[{"minx":"4312","miny":"4731","maxx":"4684","maxy":"4930"},{"minx":"4549","miny":"4506","maxx":"4665","maxy":"4582"}]}]}
+{
+  "factions": [
+    {
+      "id": "0",
+      "name": "3rd Gate Piru",
+      "color": "000000",
+      "thread": "https://forum.ls-rp.com/viewtopic.php?f=5&t=644835",
+      "turfIDs": ["1316"],
+      "turfs": ["4872 4952 4956 4952 4955 4851 4824 4819 4809 4837 4784 4835 4717 4816 4698 4818 4698 4842 4688 4866 4679 4875 4678 4945 4694 4950 4783 4952 "],
+      "turfcenter": [[4773.9285714286,4879.2857142857]],
+      "turfPolygon":[],
+      "dimensions":[[278,136]],
+      "limits": [{"minx":"4678","miny":"4816","maxx":"4956","maxy":"4952"}]
+    },
+    {
+      "id": "2",
+      "name": "Los Zetas; Fuentes y Laresca Organización",
+      "color": "6B47B7",
+      "thread": "https://forum.ls-rp.com/viewtopic.php?f=5&t=593509",
+      "turfIDs": ["1337","1350"],
+      "turfs": [
+        "4082 4131 4089 3959 4154 3957 4255 3940 4254 4136 ",
+        "4979 4033 4949 4021 4958 3959 4990 3929 5041 3919 5118 3924 5283 4008 5262 4054 5213 4091 5152 4102 5102 4076 5069 4050 5023 4038 "
+      ],
+      "turfcenter": [
+        [4166.8,4024.6],
+        [5087.6153846154,4015.6923076923]
+      ],
+      "turfPolygon": [],
+      "dimensions": [
+        [173,196],
+        [334,183]
+      ],
+      "limits": [
+        {"minx":"4082","miny":"3940","maxx":"4255","maxy":"4136"},
+        {"minx":"4949","miny":"3919","maxx":"5283","maxy":"4102"}
+      ]
+    },
+    {
+    "id": 4742,
+    "name": "Pershing Square",
+    "color": "00447c",
+    "thread": "https://discord.gg/samp",
+    "turfIDs": [
+      "4742"
+    ],
+    "turfs": [
+      "4435 4600 4434 4719 4527 4721 4526 4599"
+    ],
+    "turfcenter": [
+      [
+        4480.5,
+        4659.75
+      ]
+    ],
+    "turfPolygon": [],
+    "dimensions": [
+      [
+        93,
+        122
+      ]
+    ],
+    "limits": [
+      {
+        "minx": 4434,
+        "miny": 4599,
+        "maxx": 4527,
+        "maxy": 4721
+      }
+    ]
+  }
+  ]
+}
